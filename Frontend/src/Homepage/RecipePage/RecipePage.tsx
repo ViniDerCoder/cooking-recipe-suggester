f30d37100fdce8d45fd1b5025a9878816a393b8b
ViniDerCoder <85809337+ViniDerCoder@users.noreply.github.com>
2024-07-09 22:37:14 +0200

    Worked on recipe Page

--- Frontend/src/Homepage/RecipePage/RecipePage.tsx
+++ Frontend/src/Homepage/RecipePage/RecipePage.tsx
@@ -1,61 +1,79 @@
+import './../../ColorScheme.css';
+import './RecipePage.css';
+
 import { useEffect, useState } from "react";
-import { getIngredientsOfRecipe, getRecipeById } from "./recipeLogic";
+import { getIngredientsOfRecipe } from "./recipeLogic";
 import { Recipe } from '../../../../Backend/src/utils/types/recipe'
 import { IngredientRecipeData } from '../../../../Backend/src/utils/types/ingredient'
 
 
-export default function RecipePage(p: {recipeId: string}) {
+export default function RecipePage(p: { recipeId: string }) {
     const [recipe, setRecipe] = useState<Recipe | null | false>(null);
     const [ingredients, setIngredients] = useState<IngredientRecipeData[] | null | false>(null);
     const [isLoading, setIsLoading] = useState(true);
-  
+
     useEffect(() => {
         const loadRecipe = async () => {
             console.log(p.recipeId)
-            const recipe = await getRecipeById(p.recipeId);
-            if(recipe[0]) {
-              const ingredients = await getIngredientsOfRecipe(p.recipeId);
-              if(ingredients[0]) recipe[1].ingredients = ingredients[1];
-              else setIngredients(false);
+            const recipe: [boolean, Recipe] = [true, {
+                name: "Pfannkuchen",
+                description: "Leckere Pfannkuchen süß und herzhaft gefüllt.",
+                instructions: ["Instruction 1", "Instruction 2"],
+                imageUrl: "https://img.chefkoch-cdn.de/rezepte/2529831396465550/bilder/1509532/crop-960x540/pfannkuchen-crepe-und-pancake.jpg",
+                id: p.recipeId,
+                createdAt: new Date(),
+                createdById: "someId",
+                waitingTime: 111,
+                servings: 2,
+                cookingTime: 22,
+                public: true,
+                typeId: "someId"
+            } as Recipe];
+            if (recipe[0]) {
+                const ingredients = await getIngredientsOfRecipe(recipe[1].id);
+                if (ingredients[0]) setIngredients(ingredients[1]);
+                else setIngredients([{ id: "sdwass", amount: 0, unit: 'some' }]);
 
-              setRecipe(recipe[1]);
+                setRecipe(recipe[1]);
             }
             else setRecipe(false);
 
             setIsLoading(false);
         };
-  
+
         loadRecipe();
     }, [p.recipeId]);
 
     if (isLoading) {
-        return <div>Loading...</div>; // Or any other loading indicator
+        return <div>Loading...</div>;
     }
 
-    if(recipe === false || recipe === null) {
+    if (recipe === false || recipe === null) {
         return <div>Recipe not found</div>
     }
 
-    if(ingredients === false || ingredients === null) {
-      return <div>Ingredients not loaded</div>
-  }
+    if (ingredients === false || ingredients === null) {
+        return <div>Ingredients not loaded</div>
+    }
 
     return (
-        <div>
-            <h1>{recipe.name}</h1>
-            <p>{recipe.description}</p>
-            {recipe.imageUrl ? <img src={recipe.imageUrl} alt={recipe.name} /> : null}
-            <ul>
-            </ul>
-            <p>{recipe.instructions.map((instr, index) => {
+        <div className="recipe-page">
+            <div className="recipe-page-left">
+                {recipe.imageUrl ? <img src={recipe.imageUrl} alt={recipe.name} className="recipe-page-image" /> : null}
+                <h1 className="recipe-page-name">{recipe.name}</h1>
+                <p className="recipe-page-description">{recipe.description}</p>
+                <div className="recipe-page-ingredients">
+                    <h2 className="recipe-page-ingredients-title">Ingredients</h2>
+                    <ul className="recipe-page-ingredients-list">
+                        {ingredients.map((ingredient) => {
+                            return <li className="recipe-page-ingredients-list-item">{ingredient.amount} {ingredient.unit ? ingredient.unit : ''} of {ingredient.id}</li>
+                        })}
+                    </ul>
+                </div>
+            </div>
+            <div className='recipe-page-instructions'>{recipe.instructions.map((instr, index) => {
                 return <p>{index + 1}. {instr}</p>
-            })}</p>
-            <h2>Ingredients</h2>
-            <ul>
-                {ingredients.map((ingredient) => {
-                    return <li>{ingredient.amount} {ingredient.unit ? ingredient.unit : ''} of {ingredient.id}</li>
-                })}
-            </ul>
+            })}</div>
         </div>
     )
 }
