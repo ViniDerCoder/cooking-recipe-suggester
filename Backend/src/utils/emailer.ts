77057b3e59239cc73bb7518db7521c57d5cbd7b2
ViniDerCoder <85809337+ViniDerCoder@users.noreply.github.com>
2024-07-11 18:06:33 +0200

    Changed emailer

--- Backend/src/utils/emailer.ts
+++ Backend/src/utils/emailer.ts
@@ -1,43 +1,43 @@
 import nodemailer from 'nodemailer';
 import { config } from 'dotenv'
+import nodeMailjet from 'node-mailjet'
 
 config();
 
-if(!process.env['EMAILER_SERVICE'] || !process.env['EMAILER_USER_ADDRESS'] || !process.env['EMAILER_USER_PASSWORD']) throw new Error('Please provide the Emailer Service, User Address, and User Password in the .env file');
+if (!process.env['MAILJET_API_KEY'] || !process.env['MAILJET_API_SECRET'] || !process.env["EMAILER_USER_ADDRESS"]) throw new Error('Please provide the Emailer API key and secret in the .env file');
 
-const transporter = nodemailer.createTransport({
-    service: process.env.EMAILER_SERVICE, 
-    auth: {
-      user: process.env.EMAILER_USER_ADDRESS, 
-      pass: process.env.EMAILER_USER_PASSWORD
-    }
-});
+const mailjet = new nodeMailjet({
+    apiKey: process.env.MAILJET_API_KEY,
+    apiSecret: process.env.MAILJET_API_SECRET
+})
+
 
 export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-export function sendMail(to: Array<string>, subject: string, text: string, html?: string) {
-    return new Promise((resolve, reject) => {
-        if(!to.some((email) => email.match(emailRegex))) return resolve("Invalid email");
-        
-        transporter.sendMail({ from: process.env["EMAILER_USER_ADDRESS"], to: to, subject: subject, text: text, html: html}, function(error, info){
-            if (error) {
-                console.log(error);
-                return resolve("Failed to send email");
-            } else {
-                return resolve(undefined);
-            }
-        });
-    });
+export async function sendMail(to: Array<string>, subject: string, text: string, html?: string) {
+    return new Promise(async (resolve, reject) => {
+        if (!to.some((email) => email.match(emailRegex))) return resolve("Invalid email");
+
+        try {
+            const request = await mailjet.post('send', { version: 'v3.1' }).request({
+                Messages: [
+                    {
+                        From: {
+                            Email: process.env.EMAILER_USER_ADDRESS,
+                            Name: 'Cooking Recipe Suggester'
+                        },
+                        To: to.map((email) => ({ Email: email })),
+                        Subject: subject,
+                        TextPart: text,
+                        HTMLPart: html
+                    }
+                ]
+            })
+            resolve(request.body)
+        } catch (e) {
+            return reject(e)
+        }
+    })
 }
 
-export async function sendVerificationEmail(email: string, verificationCode: string) {}
-
-
-transporter.verify(function(error, success) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Server is ready to take our messages");
-        sendMail(["4ygpvpgktb@privaterelay.appleid.com"], "Test", "Test").then(console.log).catch(console.log);
-    }
-});
+export async function sendVerificationEmail(email: string, verificationCode: string) { }
