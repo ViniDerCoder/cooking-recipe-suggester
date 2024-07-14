dc657b2169da855e94c5b6f1be7804df7e049730
ViniDerCoder <85809337+ViniDerCoder@users.noreply.github.com>
2024-07-14 18:34:28 +0200

    Finished recipe preview and worked on homepage

--- Frontend/src/Homepage/Homepage.tsx
+++ Frontend/src/Homepage/Homepage.tsx
@@ -16,7 +16,7 @@ export default function Homepage() {
                     <div className="homepage-content-recipes-suggestions">
                         <div className="homepage-content-recipes-suggestions-header">Vorschläge</div>
                         <div className="homepage-content-recipes-suggestions-list">
-                            <RecipePreview showEditButton={false} rating={4.5} />
+                            <RecipePreview name='Käsekuchen' imageUrl='https://www.einfachbacken.de/sites/einfachbacken.de/files/styles/full_width_tablet_4_3/public/2022-04/lotus_cheesecake_2.jpeg?h=4521fff0&itok=M7V3ZxHJ' showEditButton={true} rating={7} recipeId='2302335d-2561-4334-9136-6e383ef53fca' />
                         </div>
                     </div>
                     <div className="homepage-content-recipes-own">
--- Frontend/src/Homepage/RecipePreview/RecipePreview.tsx
+++ Frontend/src/Homepage/RecipePreview/RecipePreview.tsx
@@ -1,19 +1,44 @@
 import '../../ColorScheme.css';
+import Tooltip from '../../Defaults/Tooltip/Tooltip';
 import './RecipePreview.css';
 
+import { FaRegStar, FaStar, FaStarHalfAlt } from 'react-icons/fa';
+import { MdModeEdit } from "react-icons/md";
 
 
 
-export default function RecipePreview(p: { imageUrl?: string, showEditButton: boolean, rating?: number }) {
+
+export default function RecipePreview(p: { imageUrl?: string, showEditButton: boolean, rating?: number, name: string, recipeId: string }) {
     return (
-        <div className="recipe-preview">
+        <div className="recipe-preview"
+            onClick={() => {
+                window.location.href = `./#/recipe/${p.recipeId}`;
+            }}
+        >
             <div className='recipe-preview-image-cover'>
-                <img className="recipe-preview-image" alt="" src={p.imageUrl ? p.imageUrl : "https://fps.cdnpk.net/home/cover/image-12-sm.webp?w=438&h=438"} ></img>
+                <img className="recipe-preview-image" alt="" src={p.imageUrl ? p.imageUrl : "https://cdn3.iconfinder.com/data/icons/design-n-code/100/272127c4-8d19-4bd3-bd22-2b75ce94ccb4-512.png"} ></img>
             </div>
             <div className="recipe-preview-content">
-                <div className="recipe-preview-name">Name</div>
-                <div className="recipe-preview-rating">{p.rating ? p.rating : 'Keine Bewertung'}</div>
-                {p.showEditButton ? <div className="recipe-preview-edit">Bearbeiten</div> : null}
+                <div className="recipe-preview-rating">{[...Array(5)].map((e, i) => {
+                    return <div className="recipe-preview-rating-star">
+                        {p.rating ? p.rating >= i * 2 + 2 ? <FaStar color='gold' size={"1.2rem"} /> : p.rating >= i * 2 + 1 ? <FaStarHalfAlt color='gold' size={"1.2rem"} /> : <FaRegStar color='gold' size={"1.2rem"} /> : <FaRegStar color='gold' size={"1.2rem"} />}
+                    </div>
+                })}
+                </div>
+                <div className='recipe-preview-content-row'>
+                    <Tooltip
+                        element={<div className="recipe-preview-name" >{p.name}</div>}
+                        message={p.name}
+                    />
+                    {p.showEditButton ? <div className="recipe-preview-edit"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            window.location.href = `./#/recipe/${p.recipeId}/editor`;
+                        }}
+                    >
+                        <MdModeEdit size={"1.2rem"} />
+                    </div> : null}
+                </div>
             </div>
         </div>
     );
